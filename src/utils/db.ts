286e5af777ae38c30f729603e4b49e751ec8a0d2
Jordan Baron <jordannbaron@gmail.com>
2021-11-23 19:38:17 -0500

    Use cached connection when possible

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,13 +5,15 @@ dotenv.config();
 const username = process.env['DB_UNAME'];
 const password = process.env['DB_PASSWORD'];
 
-export const database = {
-	connectionString: `mongodb+srv://${username}:${password}@ctrlv.tp1ux.mongodb.net/ctrlv?retryWrites=true&w=majority`,
+let cachedDb = null;
 
-	connect: async function () {
-		await mongoose.connect(this.connectionString);
-	},
-	disconnect: async () => {
-		await mongoose.disconnect();
-	}
+const connectionString = `mongodb+srv://${username}:${password}@ctrlv.tp1ux.mongodb.net/ctrlv?retryWrites=true&w=majority`;
+
+export const connectToDb = async () => {
+	if (cachedDb) return cachedDb;
+
+	const db = await mongoose.connect(connectionString);
+	cachedDb = db;
+
+	return db;
 };
