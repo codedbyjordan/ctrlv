f1e7a87f96b38b9f1fac81efe4b68c72ba49ced1
Jordan Baron <jordannbaron@gmail.com>
2021-12-03 10:10:48 -0500

    Add environment variables for connection URL and database name

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const username = process.env['DB_UNAME'];
-const password = process.env['DB_PASSWORD'];
+const username: string = process.env['DB_UNAME'];
+const password: string = process.env['DB_PASSWORD'];
+const connectionUrl: string = process.env['CONNECTION_URL'];
+const dbName: string = process.env['DB_NAME'];
 
 let cachedDb = null;
 
-const connectionString = `mongodb+srv://${username}:${password}@ctrlv.tp1ux.mongodb.net/ctrlv?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${username}:${password}@${connectionUrl}/${dbName}?retryWrites=true&w=majority`;
 
 export const connectToDb = async () => {
 	if (cachedDb) return cachedDb;
